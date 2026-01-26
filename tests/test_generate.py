3c9825e6cf5d912ad28376a12f72b514cafef6ce
Farzin Kazemzadeh <itisFarzin@proton.me>
2026-01-26 14:48:16 +0330

    test(generate): refactor and add test for handle_devices
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@proton.me>

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,4 +1,4 @@
-from generate import Config, is_custom_bind, handle_volumes
+from generate import Config, is_custom_bind, handle_volumes, handle_devices
 
 
 def test_is_custom_bind():
@@ -9,7 +9,8 @@ def test_is_custom_bind():
 def test_handle_volumes():
     config = Config()
     volumes = ["/volume", "/volume2"]
-    result = handle_volumes(config, {}, "container", volumes, [])
+    container = {}
+    result = handle_volumes(config, container, "container", volumes, [])
     assert result == [
         "${BIND_PATH}/container/volume:/volume",
         "${BIND_PATH}/container/volume2:/volume2",
@@ -19,22 +20,31 @@ def test_handle_volumes():
 def test_handle_volumes_with_custom_binds():
     config = Config()
     volumes = ["/volume:/volume", "/volume2:/volume2"]
-    result = handle_volumes(config, {}, "container", volumes, [])
+    container = {}
+    result = handle_volumes(config, container, "container", volumes, [])
     assert result == ["/volume:/volume", "/volume2:/volume2"]
 
 
 def test_handle_volumes_with_custom_binds_and_mount_options():
     config = Config()
     volumes = ["/volume:/volume:ro", "/volume2:/volume2:rw"]
-    result = handle_volumes(config, {}, "container", volumes, [])
+    container = {}
+    result = handle_volumes(config, container, "container", volumes, [])
     assert result == ["/volume:/volume:ro", "/volume2:/volume2:rw"]
 
 
 def test_handle_volumes_with_mount_options_and_custom_name():
     config = Config()
     volumes = ["/volume:ro;config", "/volume2:rw;data"]
-    result = handle_volumes(config, {}, "container", volumes, [])
+    container = {}
+    result = handle_volumes(config, container, "container", volumes, [])
     assert result == [
         "${BIND_PATH}/container/config:/volume:ro",
         "${BIND_PATH}/container/data:/volume2:rw",
     ]
+
+
+def test_handle_devices():
+    devices = ["/device", "/device2:/device2"]
+    result = handle_devices(devices)
+    assert result == ["/device:/device", "/device2:/device2"]
